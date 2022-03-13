66805b95ba2c781393d39bc9457712394065266a
Reza Snowdon <rs@mage.me.uk>
2022-03-13 17:21:42 +0000

    Fix tests to work with gitlab ci

--- tests/test_cuppa.py
+++ tests/test_cuppa.py
@@ -1,9 +1,18 @@
 import pytest
 from cuppa.config import Config
 
+config_file = 'cuppa.ini'
+
 result = {
     'primary_action': 'pull',
     'secondary_action': 'db',
+}
+
+file_result = {
+    'hostname': 'example.com',
+    'username': 'user',
+    'password': 'pass',
+    'remote_files_folder': '/home/user/public_html'
 }
 
 test_argv = [
@@ -13,21 +22,13 @@ test_argv = [
 
 
 def test_get_cli_arguments():
-    """"  Testing command line arguments are in correct format
-
-    """
+    """"  Basic test to check cuppa processes command line arguments """
 
     config = Config(test_argv)
     assert(result == config.get_cli_args())
 
 
-# def test_read_config_file():
-#     config = Config(test_argv)
-#     assert (result == config.read_file())
-#
-#
-# def test_save_config_file():
-#     config = Config(test_argv)
-#     assert (config.save_file())
-
-
+def test_read_config_file():
+    """"  Basic test to check cuppa can read the config file for a project """
+    config = Config(test_argv)
+    assert (file_result == config.read_file())
