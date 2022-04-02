bc1a2b850942dddf4e4dffbd5cd440390e523315
Reza Snowdon <rs@mage.me.uk>
2022-04-02 23:10:41 +0100

    Use specified remote tmp directory rather than using the system on in the Filemanager class

--- cuppa/filemanager.py
+++ cuppa/filemanager.py
@@ -1,16 +1,15 @@
-import tempfile
 
 
 class FileManager:
     def __init__(self, config_data, connection):
         self.config_data = config_data
-        self.tmp_dir = tempfile.gettempdir()
+        self.tmp_dir = config_data['remote_temporary_folder']
         self.connection = connection
         self.config_filename = 'wp-config.php'
 
     def archive(self, location='remote'):
         if location == 'remote':
-            archive_path = self.tmp_dir + '/' + 'cuppa-archive.zip '
+            archive_path = self.tmp_dir + '/cuppa-archive.zip '
             command = 'zip -r ' + archive_path + self.config_data['remote_files_folder'] \
                       + ' ' + self.config_data['remote_sql_folder']
 
