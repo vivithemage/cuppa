dc7ee03e39e69d5b085d04bd4a04557aae87de85
Reza Snowdon <rs@mage.me.uk>
2022-05-31 21:32:13 +0100

    fix typos in imports for push and pull commands. Also check arguments earlier on.

--- main.py
+++ main.py
@@ -7,12 +7,14 @@ from cuppa.filetransport import FileTransport
 
 from cuppa.command.archive import CommandArchive
 from cuppa.command.push import CommandPush
-from cuppa.command.pull import CommandPush
+from cuppa.command.pull import CommandPull
 
 
 def cuppa():
     config = Config(sys.argv)
     config_data = config.read_file()
+
+    arguments = config.get_cli_args()
 
     ssh_connector = CuppaSSH(config_data)
     connection = ssh_connector.open_connection()
@@ -21,17 +23,15 @@ def cuppa():
     init = Init(config_data, file_transport)
     init.startup()
 
-    arguments = config.get_cli_args()
-
     if arguments['primary_action'] == 'archive':
         command = CommandArchive(config_data, connection, file_transport)
 
     if arguments['primary_action'] == 'pull':
         if arguments['secondary_action'] == 'db':
-            command = CommandPush(config_data, connection, file_transport, ['db'])
+            command = CommandPull(config_data, connection, file_transport, ['db'])
 
         if arguments['secondary_action'] == 'files':
-            command = CommandPush(config_data, connection, file_transport, ['files'])
+            command = CommandPull(config_data, connection, file_transport, ['files'])
 
     if arguments['primary_action'] == 'push':
         if arguments['secondary_action'] == 'db':
