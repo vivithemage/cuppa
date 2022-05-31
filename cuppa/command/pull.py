d8d5af3af8c29c700a01a36278d439ffc068d3aa
Reza Snowdon <rs@mage.me.uk>
2022-05-31 21:29:53 +0100

    Notes for process for pull

--- cuppa/command/pull.py
+++ cuppa/command/pull.py
@@ -1,6 +1,31 @@
 from . generic import CommandGeneric
+from cuppa.projectdatabase import ProjectDatabase
 
 
-class CommandPush(CommandGeneric):
-    pass
+class CommandPull(CommandGeneric):
+    def run(self):
+        if self.args[0] == 'db':
+            print("pulling db")
+            """ Exporting database on remote server """
+            database = ProjectDatabase(self.config_data, self.connection)
+            # remote_sql_file_path = database.export('remote')
+            # self.file_transport.download(local_path, remote_path)
+
+            """ Download SQL file to tmp folder """
+
+            """ Extract database credentials from config """
+
+            """ Make new database """
+
+            """ Import SQL file """
+
+            """ Change wp-config to use new database. """
+
+        if self.args[0] == 'files':
+            print("pulling files")
+
+            """ Download archive """
+
+            """ Move all files other than wp-config to public_html """
+
 
